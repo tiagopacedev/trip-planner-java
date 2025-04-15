8c06d72b06475bade1af01cdfda5ad0bc70f2ada
Tiago Pace <tiagopace.dev@gmail.com>
2025-04-15 13:13:02 -0300

    Initial Commit

---
+++ src/test/java/com/trip_planner/trip_planner/TripPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip_planner.trip_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
